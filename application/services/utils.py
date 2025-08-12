5d43b95f51232119adbeb1005878f00fb01db3de
Ichiro.S <sey323@users.noreply.github.com>
2025-08-13 00:01:33 +0900

    💚Formatの適応/利用指定ないファイルを削除/環境変数更新/task.jsonを追加  (#20)

--- application/services/utils.py
+++ application/services/utils.py
@@ -6,13 +6,34 @@ def relative_date_tool(text, base_ts):
     """相対的な日付表記を絶対表記に変更するためのヘルパー関数"""
     base_date = datetime.fromtimestamp(base_ts)
     patterns = [
-        (r"来週月曜日", lambda d: d + timedelta(days=(7 - d.weekday() + 0) % 7 + 7)),
-        (r"来週火曜日", lambda d: d + timedelta(days=(7 - d.weekday() + 1) % 7 + 7)),
-        (r"来週水曜日", lambda d: d + timedelta(days=(7 - d.weekday() + 2) % 7 + 7)),
-        (r"来週木曜日", lambda d: d + timedelta(days=(7 - d.weekday() + 3) % 7 + 7)),
-        (r"来週金曜日", lambda d: d + timedelta(days=(7 - d.weekday() + 4) % 7 + 7)),
-        (r"来週土曜日", lambda d: d + timedelta(days=(7 - d.weekday() + 5) % 7 + 7)),
-        (r"来週日曜日", lambda d: d + timedelta(days=(7 - d.weekday() + 6) % 7 + 7)),
+        (
+            r"来週月曜日",
+            lambda d: d + timedelta(days=(7 - d.weekday() + 0) % 7 + 7),
+        ),
+        (
+            r"来週火曜日",
+            lambda d: d + timedelta(days=(7 - d.weekday() + 1) % 7 + 7),
+        ),
+        (
+            r"来週水曜日",
+            lambda d: d + timedelta(days=(7 - d.weekday() + 2) % 7 + 7),
+        ),
+        (
+            r"来週木曜日",
+            lambda d: d + timedelta(days=(7 - d.weekday() + 3) % 7 + 7),
+        ),
+        (
+            r"来週金曜日",
+            lambda d: d + timedelta(days=(7 - d.weekday() + 4) % 7 + 7),
+        ),
+        (
+            r"来週土曜日",
+            lambda d: d + timedelta(days=(7 - d.weekday() + 5) % 7 + 7),
+        ),
+        (
+            r"来週日曜日",
+            lambda d: d + timedelta(days=(7 - d.weekday() + 6) % 7 + 7),
+        ),
         (r"今週月曜日", lambda d: d + timedelta(days=(0 - d.weekday()) % 7)),
         (r"今週火曜日", lambda d: d + timedelta(days=(1 - d.weekday()) % 7)),
         (r"今週水曜日", lambda d: d + timedelta(days=(2 - d.weekday()) % 7)),
@@ -26,9 +47,11 @@ def relative_date_tool(text, base_ts):
         (r"今日", lambda d: d),
     ]
     for pat, func in patterns:
+
         def repl(match):
             target = func(base_date)
             return f"{target.strftime('%Y/%m/%d(%a)')}"
+
         text = re.sub(pat, repl, text)
     return text
 
@@ -36,4 +59,4 @@ def relative_date_tool(text, base_ts):
 def get_today_date_string():
     """今日のUTC日付をYYYY-MM-DD形式で取得するヘルパー関数"""
     today = datetime.utcnow()
-    return today.strftime('%Y-%m-%d')
+    return today.strftime("%Y-%m-%d")
